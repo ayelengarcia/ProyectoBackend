9995d80b2dec996112c5da67d2345f25551a59d5
Ayelen Garcia <ayelengarcia7@hotmail.com>
2023-09-28 15:49:14 -0300

    add permisos admin

---
+++ src/public/js/admin-products.js
@@ -0,0 +1,81 @@
+const btnActualizar = document.getElementById("btnActualizar");
+const btnEliminar = document.getElementById("btnEliminar");
+
+//ACTUALIZAR PRODUCTO
+function updateProduct() {
+  const form = document.getElementById("updateProducts");
+  const responseContainer = document.getElementById("response-container");
+  const formData = new FormData(form);
+
+  const productId = formData.get("inputID");
+  const url = `/api/products/${productId}`;
+
+  const data = {
+    title: formData.get("title"),
+    description: formData.get("description"),
+    thumbnail: formData.get("thumbnail"),
+    price: formData.get("price"),
+    stock: formData.get("stock"),
+    code: formData.get("code"),
+  };
+
+  fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Error al actualizar el producto");
+      }
+    })
+    .then((responseData) => {
+      form.reset();
+      console.log("Producto actualizado exitosamente", responseData);
+      responseContainer.innerHTML = "Producto Actualizado";
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+btnActualizar.addEventListener("click", function () {
+  updateProduct();
+});
+
+//ELIMINAR PRODUCTO
+function deleteProduct() {
+  const form = document.getElementById("deleteProducts");
+  const responseContainer = document.getElementById("container-delete");
+  const formData = new FormData(form);
+
+  const productId = formData.get("idDelete");
+  const url = `/api/products/${productId}`;
+
+  fetch(url, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Error al eliminar el producto");
+      }
+    })
+    .then((responseData) => {
+      console.log("Producto eliminado exitosamente", responseData);
+      form.reset();
+      responseContainer.innerHTML = "Producto Eliminado";
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+btnEliminar.addEventListener("click", function () {
+  deleteProduct();
+});
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -6,18 +6,34 @@ import {
   getProducts,
   updatedProductById,
 } from "../controllers/products.controller.js";
+import { authorizationRol, authorizationStrategy } from "../utils.js";
 
 const router = Router();
 
 router.get("/products", getProducts);
 // http://127.0.0.1:8080/api/products?limit=5
 
-router.post("/products", addProducts);
+router.post(
+  "/products",
+  authorizationStrategy("jwt", { session: false }),
+  authorizationRol("Admin"),
+  addProducts
+);
 
 router.get("/products/:pid", getProductById);
 
-router.put("/products/:pid", updatedProductById);
+router.put(
+  "/products/:pid",
+  authorizationStrategy("jwt", { session: false }),
+  authorizationRol("Admin"),
+  updatedProductById
+);
 
-router.delete("/products/:pid", deletedProduct);
+router.delete(
+  "/products/:pid",
+  authorizationStrategy("jwt", { session: false }),
+  authorizationRol("Admin"),
+  deletedProduct
+);
 
 export default router;
