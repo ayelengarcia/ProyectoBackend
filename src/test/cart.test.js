9a5bf46f6067a352434fe7bfc3176ba6443d931f
Ayelen Garcia <ayelengarcia7@gmail.com>
2023-11-11 14:30:15 -0300

    test unitarios

--- src/test/cart.test.js
+++ src/test/cart.test.js
@@ -1,4 +1,5 @@
 import Cart from "../Dao/mongoManager/cart.mongo.js";
+import Product from "../Dao/mongoManager/product.mongo.js";
 import Assert from "assert";
 import mongoose from "mongoose";
 import config from "../config/config.js";
@@ -38,4 +39,54 @@ describe("Testing Cart DAO", () => {
 
     assert.deepStrictEqual(result.products, []);
   });
+
+  it("El DAO debe poder obtener un Carrito por ID", async function () {
+    this.timeout(5000);
+    let cart = [];
+
+    const cartDao = new Cart();
+    const createdCart = await cartDao.createCart(cart);
+
+    // Obtener cart por ID
+    const result = await cartDao.getCartById(createdCart._id);
+
+    assert.strictEqual(result._id.toString(), createdCart._id.toString());
+  });
+
+  it("El DAO debe poder eliminar un Carrito por ID", async function () {
+    this.timeout(5000);
+    let cart = [];
+  
+    const cartDao = new Cart();
+    const createdCart = await cartDao.createCart(cart);
+  
+    const result = await cartDao.deleteCartById(createdCart._id);
+    assert.ok(result, true);
+  });
+
+  it("El DAO debe poder agregar un Producto al Carrito", async function () {
+    this.timeout(5000);
+    let product = {
+      title: "Producto para agregar al carrito",
+      thumbnail: "Imagen",
+      description: "Descripción",
+      stock: 10,
+      price: 100,
+    };
+
+    const productDao = new Product();
+    const createdProduct = await productDao.addProducts(product);
+
+    const cartDao = new Cart();
+    const createdCart = await cartDao.createCart();
+
+    // Agregar el producto al carrito
+    const result = await cartDao.addProductCart(createdCart._id, createdProduct._id, 2);
+
+    // Verificar que se agrega
+    assert.strictEqual(result.products.length, 1);
+    assert.strictEqual(result.products[0].pid._id.toString(), createdProduct._id.toString());
+    assert.strictEqual(result.products[0].quantity, 2);
+  });
+  
 });
--- src/test/product.test.js
+++ src/test/product.test.js
@@ -1,6 +1,6 @@
 import Product from "../Dao/mongoManager/product.mongo.js";
 import Assert from "assert";
-import chai from "chai";
+// import chai from "chai";
 import mongoose from "mongoose";
 import config from "../config/config.js";
 
@@ -12,7 +12,7 @@ mongoose
 //Nativo de node
 const assert = Assert.strict;
 //Chai Libreria mas usada
-const expect = chai.expect;
+// const expect = chai.expect;
 
 describe("Testing Product DAO", () => {
   it("El DAO debe poder obtener los Productos", async function () {
@@ -21,7 +21,7 @@ describe("Testing Product DAO", () => {
     const result = await productDao.getProducts();
 
     assert.strictEqual(Array.isArray(result), true);
-    expect(result).to.be.deep.equal([]);
+    // expect(result).to.be.deep.equal([]);
   });
 
   it("El DAO debe poder crear Productos", async function () {
@@ -39,4 +39,70 @@ describe("Testing Product DAO", () => {
 
     assert.ok(result._id);
   });
+
+  it("El DAO debe poder obtener un Producto por ID", async function () {
+    this.timeout(5000);
+    let product = {
+      title: "Producto a obtener por ID",
+      thumbnail: "Imagen",
+      description: "Descripción",
+      stock: 10,
+      price: 100,
+    };
+
+    const productDao = new Product();
+    const createdProduct = await productDao.addProducts(product);
+
+    // Obtener producto por ID
+    const result = await productDao.getProductById(createdProduct._id);
+
+    assert.strictEqual(result._id.toString(), createdProduct._id.toString());
+    assert.strictEqual(result.title, product.title);
+  });
+
+  it("El DAO debe poder actualizar un Producto por ID", async function () {
+    this.timeout(5000);
+    let product = {
+      title: "Producto a actualizar por ID",
+      thumbnail: "Imagen",
+      description: "Descripción",
+      stock: 10,
+      price: 100,
+    };
+  
+    const productDao = new Product();
+    const createdProduct = await productDao.addProducts(product);
+  
+    // Producto actualizado
+    const updatedProductData = {
+      title: "Producto Actualizado",
+      stock: 20,
+      price: 150,
+    };
+  
+    const updatedProduct = await productDao.updatedProductById(createdProduct._id, updatedProductData);
+  
+    assert.strictEqual(updatedProduct.title, updatedProductData.title);
+    assert.strictEqual(updatedProduct.stock, updatedProductData.stock);
+    assert.strictEqual(updatedProduct.price, updatedProductData.price);
+  });
+  
+  it("El DAO debe poder eliminar un Producto por ID", async function () {
+    this.timeout(5000);
+    let product = {
+      title: "Producto a eliminar por ID",
+      thumbnail: "Imagen",
+      description: "Descripción",
+      stock: 10,
+      price: 100,
+    };
+  
+    const productDao = new Product();
+    const createdProduct = await productDao.addProducts(product);
+  
+    const result = await productDao.deleteProduct(createdProduct._id);
+    assert.ok(result, true);
+  });
+  
+
 });
